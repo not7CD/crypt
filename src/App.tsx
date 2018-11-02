8ca0e68486d265fc1ed0fb82d491d9ffc5018ec1
Norbert Szulc <norbert@not7cd.net>
2018-11-02 23:41:41 +0100

    Working Base64

--- src/App.tsx
+++ src/App.tsx
@@ -22,34 +22,19 @@ class ReturnButton extends Component {
   }
 }
 
-interface IModeProps {
 
-}
-
-interface IModeState {
-  rSelected: number
-}
-
-class ModeButtonGroup extends Component<IModeProps, IModeState>{
-  constructor (props: IModeProps) {
+class ModeButtonGroup extends Component<any, any>{
+  constructor (props: any) {
     super(props);
-
-    this.state = { rSelected: 1 };
-
-    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
-  }
-
-  onRadioBtnClick(rSelected: any) {
-    this.setState({ rSelected });
   }
   
   render() {
     return (
        <ButtonGroup className="btn-group-toggle" data-toggle="buttons">
-         <Button className="btn-outline-primary" onClick={() => this.onRadioBtnClick(1)} active={this.state.rSelected === 1}>
+         <Button className="btn-outline-primary" onClick={() => this.props.onChange("encode")} active={this.props.mode === "encode"}>
            Encode
          </Button>
-         <Button className="btn-outline-primary" onClick={() => this.onRadioBtnClick(2)} active={this.state.rSelected === 2}>
+         <Button className="btn-outline-primary" onClick={() => this.props.onChange("decode")} active={this.props.mode === "decode"}>
            Decode
          </Button>
        </ButtonGroup>
@@ -57,28 +42,110 @@ class ModeButtonGroup extends Component<IModeProps, IModeState>{
   }
 }
 
+class TextArea extends Component<any, any> {
 
-class ToolBase64 extends Component {
+  render() {
+    return (
+       <FormGroup>
+         <Label for={this.props.id}>{this.props.label}</Label>
+         <Input 
+           type="textarea" 
+           name="text" id={this.props.id} 
+           onChange={this.props.onChange} 
+           invalid={!this.props.valid}
+          />
+       </FormGroup>
+    );
+  }
+}
+
+class OutputTextArea extends Component<any, any> {
+
+  render() {
+    return (
+       <FormGroup>
+         <Label for={this.props.id}>{this.props.label}</Label>
+         <Input type="textarea" name="text" id={this.props.id} value={this.props.value} readOnly/>
+       </FormGroup>
+    );
+  }
+}
+
+
+class ToolBase64 extends Component<any, any> {
   constructor (props: any) {
     super(props);
-    
+
+    this.state = {input: "", mode: "encode", output: "", validInput: true}
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleModeChange = this.handleModeChange.bind(this);
+
+  }
+
+  handleInputChange(event: any) {
+    this.setState(
+      {input: event.target.value}, 
+      () => this.setOutput()
+     )
+  }
+
+  handleModeChange(mode: string) {
+    this.setState(
+      {mode},
+      () => this.setOutput()
+     )
+  }
+
+  setOutput() {
+    let output
+    if (this.state.mode === "encode") {
+      output = this.encode(this.state.input)
+      this.setState({
+        output: output, 
+        validInput: true})
+    }
+    else if(this.state.mode === "decode") {
+      try {
+        output = this.decode(this.state.input)
+        this.setState({
+          output: output, 
+          validInput: true})
+      } catch(e) {
+        this.setState({
+          validInput: false
+        })
+      }
+    }
+  }
+
+
+  encode(plaintext: string) {
+    return btoa(plaintext)
+
+  }
+
+  decode(encodedtext: string) {
+    return atob(encodedtext)
   }
 
   render() {
      return (
        <div>
-         <h1>Base 64</h1>
+       <h1>Base 64</h1>
+       <div className="card">
+       <div className="card-body">
+         <TextArea id="input" label="Input" onChange={this.handleInputChange} valid={this.state.validInput}/>
          <FormGroup>
-           <Label for="exampleText">Input</Label>
-           <Input type="textarea" name="text" id="exampleText" />
+           <ModeButtonGroup onChange={this.handleModeChange} mode={this.state.mode}/>
          </FormGroup>
-         <FormGroup>
-           <ModeButtonGroup />
-         </FormGroup>
-         <FormGroup>
-           <Label for="exampleText">Output</Label>
-           <Input type="textarea" name="text" id="exampleText" />
-         </FormGroup>
+         <OutputTextArea 
+           id="output" 
+           label="Output" 
+           value={this.state.output} 
+          />
+       </div>
+       </div>
        </div>
        );
   }
